db6a1de0dbb02fff1d22b26d27bbd831fbf2a0eb
nikhil <nikhilsai.v@gmail.com>
2021-03-29 00:35:33 +0530

    mobile responsive

--- src/dashBoardComponent/dashBoardComponent.js
+++ src/dashBoardComponent/dashBoardComponent.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import styles from './dashBoardComponent.module.css';
+import  './dashBoardComponent.css';
 import profile from './../assets/profile.png'
 import trash from './../assets/trash-solid.svg'
 import edit from './../assets/pen-solid.svg'
@@ -31,61 +31,6 @@ function DashBoardComponent() {
             backgroundColor: "#F4F4F6",
             height: "700px",
             width: "100%",
-        },
-        summary: {
-
-
-            row: {
-                padding: "1% 11%",
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between"
-            },
-            summaryCard: {
-                width: "304px",
-                height: "158px",
-                background: "#FFFFFF",
-                boxShadow: "0px 3px 6px #0000000A",
-                borderRadius: "12px",
-                opacity: 1,
-                padding: "24px 24px",
-                textAlign: "left",
-                color: "#8F9EA2",
-            },
-            cardTitle: {
-                fontSize: "20px",
-                textAlign: "left",
-                letterSpacing: "0px",
-                color: "#537178",
-                opacity: 1,
-                fontWeight: 400
-            },
-            listElement: {
-                listStylePosition: "inside",
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                textOverflow: "ellipsis"
-            },
-            list: {
-                paddingInlineStart: "0px",
-                fontSize: "15px"
-            },
-            editableList: {
-                listElement: {
-                    listStylePosition: "inside",
-                    whiteSpace: "nowrap",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis"
-                },
-                list: {
-                    paddingInlineStart: "0px",
-                    fontSize: "20px",
-                    listStyleType: "none"
-                },
-
-            }
-
-
         },
         notask: {
             card: {
@@ -155,21 +100,19 @@ function DashBoardComponent() {
 
     let latestTasks = (tasks.length > 3) ? tasks.slice(tasks.length - 3, tasks.length) : tasks;
     let latestTasksList = latestTasks.map(task => {
-        let completedStyle = { ...styles.summary.listElement, textDecoration: "line-through" };
         if (task.completed)
-            return (<li style={completedStyle} key={task.id}>{task.name}</li>)
+            return (<li className="list-wrap striked" key={task.id}>{task.name}</li>)
         else
-            return (<li style={styles.summary.listElement} key={task.id}>{task.name}</li>)
+            return (<li className="list-wrap" key={task.id}>{task.name}</li>)
     })
 
 
     let totalList = tasks.map(task => {
-        let completedStyle = { ...styles.summary.editableList.listElement, textDecoration: "line-through" };
         return (<li key={task.id}>
             <div className="row">
                 <input type="checkbox" onChange={(e)=> changeCompletionStatus(task.id,e.target.checked)} />
-                {!task.completed && <span style={styles.summary.editableList.listElement}>{task.name}</span>}
-                {task.completed && <span style={completedStyle}>{task.name}</span>}
+                {!task.completed && <span className="list-wrap">{task.name}</span>}
+                {task.completed && <span className="list-wrap striked">{task.name}</span>}
                 <img src={edit} height="16px" width="16px" alt="edit" />
                 <img src={trash} height="16px" width="16px" alt="trash" onClick={() => deleteTask(task.id)} />
             </div>
@@ -235,28 +178,30 @@ function DashBoardComponent() {
 
             </div>}
             { tasks.length !== 0 && <div>
-                <div style={styles.summary.row}>
-                    <div style={styles.summary.summaryCard}>
-                        <h1 style={styles.summary.cardTitle}>Tasks Completed</h1>
+                <div  className="row mr-lr-0 contentCenter">
+                    <div className="col-sm-3 col-12 text-left summaryCard">
+                        <h1 className="card-title">Tasks Completed</h1>
                         <span style={{ color: "#5285EC", fontSize: "64px" }}>{completedTasks}</span>
                         <span style={{ fontSize: "24px" }}>/{tasks.length}</span>
                     </div>
-                    <div style={styles.summary.summaryCard}>
-                        <h1 style={styles.summary.cardTitle}>Latest Created Tasks</h1>
-                        <ul style={styles.summary.list}>{latestTasksList}</ul>
+                    <div className="col-sm-3 col-12 text-left summaryCard">
+                        <h1 className="card-title">Latest Created Tasks</h1>
+                        <ul className="card-list">{latestTasksList}</ul>
                     </div>
-                    <div style={styles.summary.summaryCard}>
+                    <div className="col-sm-3 col-12 contentCenter summaryCard">
                         <Chart options={chartOptions} series={series} type="pie" height="130px" width="130px" />
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-md-5 col-12">Tasks</div>
-                    <div className="col-md-3 col-12"><input style={{width:"100%"}} type="text" placeholder="search by task name" /></div>
-                    <div className="col-md-2 col-12"><button style={{width:"100%"}}  className="btn btn-primary" onClick={() => setModalShow(true)} >+New Task</button></div>
+                <div className="row mr-lr-0">
+                    <div className="offset-sm-1 col-sm-1 col-12 pd-25 text-center">Tasks</div>
+                    <div className="offset-sm-4 col-sm-3 col-12 pd-25"><input style={{width:"100%"}} type="text" placeholder="search by task name" /></div>
+                    <div className="col-sm-2 col-12 pd-25"><button style={{width:"100%"}}  className="btn btn-primary" onClick={() => setModalShow(true)} >+New Task</button></div>
 
                 </div>
-                <div style={styles.summary.row}>
-                    <ul style={styles.summary.editableList.list}>{totalList}</ul>
+                <div className="row mr-lr-0 contentCenter">
+                    <div className="col-sm-8 col-12">
+                    <ul className="card-list list-unstyled">{totalList}</ul>
+                    </div>
                 </div>
 
             </div>}
@@ -320,108 +265,5 @@ function Header({ username }) {
         </div>
     )
 }
-
-// function NoTasks(props) {
-
-//     const style = {
-//         card: {
-//             width: "304px",
-//             height: "158px",
-//             background: "#FFFFFF 0% 0% no-repeat padding-box",
-//             boxShadow: "0px 3px 6px #0000000A",
-//             borderRadius: "12px",
-//             opacity: 1,
-//             display: "flex",
-//             flexDirection: "column",
-//             padding: "37px 64px"
-//         },
-//         heading: {
-//             textAlign: "center",
-//             font: "normal normal medium 20px/28px Montserrat",
-//             letterSpacing: "0px",
-//             color: "#537178",
-//             opacity: "1"
-//         }
-//     }
-
-//     return (
-//         <div style={style.card}>
-//             <span style={style.heading}>You have no task.</span>
-//             <button className="btn btn-primary" onClick={() => props.showAddTask(true)}>+ New Task</button>
-//         </div>
-
-//     )
-// }
-
-// function AddNewTask(props) {
-
-//     const [taskName, setTaskName] = useState("");
-//     let styles = {
-//         modal: {
-//             width: '296px',
-//             height: '195px',
-//             background: "#FFFFFF",
-//             boxShadow: "0px 3px 6px #00000029",
-//             borderRadius: "12px",
-//             opacity: 1,
-//             display: "flex",
-//             alignItems: "center",
-//             justifyContent: "center"
-//         },
-//         inputbox: {
-//             width: "100%",
-//             margin: "18px 0px",
-//             background: "#e5e5e5",
-//             border: "1px #e5e5e5",
-//             padding: "9px 16px",
-//             borderRadius: "8px",
-//             fontSize: "14px",
-//             color: "#7A7D7E",
-//         },
-//         button: {
-//             width: "100%",
-//             fontSize: "14px"
-//         },
-//         heading: {
-//             fontSize: "20px",
-//             color: "#537178"
-//         }
-//     }
-
-//     function addTask() {
-//         //   console.log(taskname);
-//         let temp = JSON.parse(localStorage.getItem("tasks"));
-//         var d = new Date();
-//         var timestamp = d.getTime();
-//         temp.push({ id: timestamp, name: taskName, completed: false });
-//         //setCount(count + 1);
-//         localStorage.setItem("tasks", JSON.stringify(temp));
-//         props.onHide(false);
-//         props.setTasks(temp);
-//     }
-
-//     return (
-//         <Modal
-//             {...props}
-//             size="sm"
-//             aria-labelledby="contained-modal-title-vcenter"
-//             centered
-//             style={{ border: "0px" }}
-
-//         >
-//             <Modal.Body style={{ padding: "24px 24px" }}>
-//                 <h4 style={styles.heading}>+ New Task</h4>
-//                 <input type="text" style={styles.inputbox} placeholder="Task Name" onChange={(e) => setTaskName(e.target.value)} />
-//                 <button style={styles.button} className="btn btn-primary" onClick={() => addTask()}>+ New Task</button>
-//             </Modal.Body>
-
-//         </Modal>
-//     );
-// }
-
-
-
-
-
 
 export default DashBoardComponent;
